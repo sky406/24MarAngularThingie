6b18f17e66f1eb821101e2155c37c249109f197a
sky406 <60670435+sky406@users.noreply.github.com>
2022-03-24 11:47:23 -0700

    added the paths for each component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogComponent } from './blog/blog.component';
+import { DocsComponent } from './docs/docs.component';
+import { EventsComponent } from './events/events.component';
+import { FeaturesComponent } from './features/features.component';
+import { HomeComponent } from './home/home.component';
+import { ResourcesComponent } from './resources/resources.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'docs',component:DocsComponent},
+  {path:'events',component:EventsComponent},
+  {path:'features',component:FeaturesComponent},
+  {path:'resourses',component:ResourcesComponent},
+  {path:'blog',component:BlogComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
